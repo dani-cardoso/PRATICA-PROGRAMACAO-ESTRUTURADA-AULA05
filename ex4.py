9745a1ff66e9b32d4a79aa12f5cb741a62dea714
Danielly Cardoso <danielly.amaral@aluno.fmpsc.edu.br>
2026-03-16 11:07:53 -0300

    Add print statement for final salary with commissions

--- ex4.py
+++ ex4.py
@@ -2,3 +2,4 @@ salarioFixo = float(input('Informe o salário fixo: '))
 valorVendas = float(input('Informe o valor das suas vendas: '))
 comissao = valorVendas * 0.04
 salarioFinal = comissao + salarioFixo 
+print(f'O valor do salario mais as comissões é {salarioFinal}')
